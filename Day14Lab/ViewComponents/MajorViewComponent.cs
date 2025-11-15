d1fa4b3b054c6a4f99a4c2b09e62b7490600b212
Admin <Admin@DMCUONG>
2025-11-15 23:28:10 +0700

    Add project files.

---
+++ Day14Lab/ViewComponents/MajorViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Day14Lab.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day14Lab.ViewComponents
+{
+    public class MajorViewComponent : ViewComponent
+    {
+        SchoolContext db;
+        List<Major> majors;
+        public MajorViewComponent(SchoolContext _context)
+        {
+            db = _context;
+            majors = db.Majors.ToList();
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("RenderMajor", majors);
+        }
+    }
+}
